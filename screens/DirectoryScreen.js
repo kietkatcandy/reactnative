9b5a0cbfd890ded7d45dfbdc3a7f34393982e9b6
Kiet <kiet.tuan.le@hotmail.com>
2023-07-19 01:41:10 -0700

    update

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -3,6 +3,8 @@ import { Tile } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from '../components/LoadingComponent';
+import * as Animatable from 'react-native-animatable'
+
 
 const DirectoryScreen = ({ navigation }) => {
     const campsites = useSelector((state) => state.campsites);
@@ -20,6 +22,12 @@ const DirectoryScreen = ({ navigation }) => {
 
     const renderDirectoryItem = ({ item: campsite }) => {
         return (
+            <Animatable.View
+                animation='fadeInRightBig'
+                duration={2000}
+                delay={1000}
+>
+            
             <Tile
                 title={campsite.name}
                 caption={campsite.description}
@@ -29,7 +37,9 @@ const DirectoryScreen = ({ navigation }) => {
                 }
                 imageSrc={{ uri: baseUrl + campsite.image }}
             />
-        );
+            </Animatable.View>
+            
+            );
     };
     return (
         <FlatList
--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -20,7 +20,7 @@ import { fetchPartners } from '../features/partners/partnersSlice';
 import { fetchCampsites } from '../features/campsites/campsitesSlice';
 import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
-
+import FavoritesScreen from './FavoritesScreen';
 
 
 const Drawer = createDrawerNavigator();
@@ -121,6 +121,28 @@ const ReservationNavigator = () => {
     );
 };
 
+const FavoritesNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name='Favorites'
+                component={FavoritesScreen}
+                options={({ navigation }) => ({
+                    title: 'Favorite Campsites',
+                    headerLeft: () => (
+                        <Icon
+                            name='heart'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                        />
+                    )
+                })}
+            />
+        </Stack.Navigator>
+    );
+};
 
 const DirectoryNavigator = () => {
     const Stack = createStackNavigator();
@@ -154,6 +176,7 @@ const DirectoryNavigator = () => {
         </Stack.Navigator>
     );
 };
+
 
 
 
@@ -245,6 +268,22 @@ const Main = () => {
                     }}
                 />
                 <Drawer.Screen
+                    name='Favorites'
+                    component={FavoritesNavigator}
+                    options={{
+                        title: 'My Favorites',
+                        drawerIcon: ({ color }) => (
+                            <Icon
+                                name='heart'
+                                type='font-awesome'
+                                size={24}
+                                iconStyle={{ width: 24 }}
+                                color={color}
+                            />
+                        )
+                    }}
+                />
+                <Drawer.Screen
                     name='About'
                     component={AboutNavigator}
                     options={{
@@ -282,7 +321,7 @@ const Main = () => {
 };
 
 const styles = StyleSheet.create({
-    drawerHeader:{
+    drawerHeader: {
         backgroundColor: '#5637DD',
         height: 140,
         alignItems: 'center',
